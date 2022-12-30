0eb6352efc5960069051dfe7d374143e4a7a7ec9
toXCustom <pawel89@gmail.com>
2022-12-30 08:18:45 +0100

    Computation Logic

--- src/sudoku/SudokuApplication.java
+++ src/sudoku/SudokuApplication.java
@@ -26,9 +26,4 @@ public class SudokuApplication extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-
-    }
 }
